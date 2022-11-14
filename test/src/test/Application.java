70d5a0738e8dc648dedf5a85f9498a85abacd7cf
student <student@rc3-14>
2022-11-14 13:45:06 +0100

    Modified bounds

--- test/src/test/Application.java
+++ test/src/test/Application.java
@@ -60,7 +60,7 @@ public class Application {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(120, 80, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		JPanel panel = new JPanel();
